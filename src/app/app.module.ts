3d924e0e5a3678eb84756a8475ec0f81b4bcba84
Andres Munoz <amun02@hotmail.com>
2019-01-20 22:24:20 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodoItemComponent,
+    TodoFooterComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styles: []
+})
+export class TodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
